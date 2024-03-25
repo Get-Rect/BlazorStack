ee559b74cb907d2e0b4555503093d47f61f56a74
John Heggelund <john.heggelund@duckscape.com>
2024-03-24 21:11:51 -0500

    styling improvements and profile component

---
+++ BlazorStack.Portal/Constants/Global.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorStack.Portal.Constants
+{
+    public static class Global
+    {
+        public static readonly string DefaultPhotoUrl = "/img/defaultUserPhoto.png";
+    }
+}
---
+++ BlazorStack.Portal/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,13 @@
+﻿using BlazorStack.Portal.Constants;
+using System.Security.Claims;
+
+namespace BlazorStack.Portal.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetPhotoUrl(this ClaimsPrincipal principal)
+        {
+            return principal.Claims.FirstOrDefault(x => x.Type.Equals("photo-url", StringComparison.OrdinalIgnoreCase))?.Value ?? Global.DefaultPhotoUrl;
+        }
+    }
+}

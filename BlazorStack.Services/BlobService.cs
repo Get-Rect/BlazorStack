c615f4c959887bda2589fd752a0babc8c42a4490
John Heggelund <john.heggelund@duckscape.com>
2024-04-02 21:42:55 -0500

    Added loading spinner. Started blob service.

---
+++ BlazorStack.Services/BlobService.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Storage.Blobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorStack.Services
+{
+    public class BlobService
+    {
+        private readonly BlobServiceClient _blobs;
+
+        public BlobService(BlobServiceClient blobs)
+        {
+            _blobs = blobs;
+        }
+
+        
+    }
+}
